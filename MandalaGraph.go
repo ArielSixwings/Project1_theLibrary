5508d42f3efb9b8ba978ced01b09384c21adb24f
ariel <arielserafi@gmail.com>
2019-03-29 11:12:39 -0300

    Beta Build MandalaRaph

--- MandalaGraph.go
+++ MandalaGraph.go
@@ -1,6 +1,15 @@
 package mygraph
 
 /**
+ * @brief      Class for letter ring.
+ */
+type LetterRing struct {
+    LetterGallery []LiteraryWork
+    Letter        int
+    LetterLabel   rune
+}
+
+/**
  * @brief      Class for literary work.
  */
 type LiteraryWork struct {
@@ -8,7 +17,7 @@ type LiteraryWork struct {
     AuthorKey  int
     PrimaryKey int
     Letter     int
-    Name       string
+    Title      string
 }
 
 /**
@@ -32,6 +41,51 @@ type InerRing struct {
  * @brief      Class for mandala graph.
  */
 type MandalaGraph struct {
-    Field []InerRing
-    size  int
+    Field     []InerRing
+    Dictonary []LetterRing
+    size      int
+}
+
+func (this *MandalaGraph) BuildMandala() bool {
+
+    this.Field = make([]InerRing, 8)
+    this.Dictonary = make([]LetterRing, 26)
+    this.size = 8
+
+    Subjects := []strings{
+        "Artes",
+        "Biologia",
+        "Computação",
+        "Física",
+        "História",
+        "Matemática",
+        "Química",
+        "Saude",
+    }
+    auxOutherRing = make([]OuterRing, 8)
+    SecondarySubjects := []strings{
+        "Análise Numérica",
+        "Arte Digital",
+        "História da Arte",
+        "Psicologia",
+        "Paleolontogia",
+        "Nutrição",
+        "Estudos Moleculares",
+        "Filosofia",
+    }
+    for i := 0; i < 8; i++ {
+        auxOuterRing.Label = SecondarySubjects[i]
+        this.Field[i].Label = Subjects[i]
+        this.Field[i].Right = auxOuterRing[i]
+        if i < 7 {
+            this.Field[i].Left = auxOuterRing[i+1]
+        } else {
+            this.Field[i].Left = auxOuterRing[0]
+        }
+    }
+    for i := 0; i < 26; i++ {
+        this.Dictonary[i].Letter = i
+        this.Dictonary[i].LetterLabel = rune(i + 65)
+        //this.Dictonary[i].LetterGallery = ReadLetter(i) // TO DO função que lé todas as obras de uma letra de um arquivo feito para a letra
+    }
 }
